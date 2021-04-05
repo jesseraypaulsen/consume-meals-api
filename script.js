7e20ca968b0ef2f20859c9e233c226a5f738bf7b
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-04-05 17:48:13 -0500

    fetch meals by category

--- script.js
+++ script.js
@@ -2,23 +2,49 @@
 // https://www.themealdb.com/api.php
 document.body.onload = consume;
 
+const categories = [];
+
 function consume() {
-  const categories = [];
   // list all categories
   fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
     .then(res => res.json())
     .then(res => {
-      //console.log(`the script has loaded! and here are the results: ${JSON.stringify(res)}`);
-      res.meals.forEach((cat, i) => {
-        categories.push(cat);
-        console.log(`${++i}. ${JSON.stringify(cat)}`);
-      });
+      processCategories(res.meals);
+      fetchMealsByCategory();
     })
     .catch(err => {
       console.warn(err);
     });
+
+  
 }
 
+function processCategories(catArray) {
+  catArray.forEach((cat, i) => {
+    categories.push(cat);
+    console.log(`${++i}. ${JSON.stringify(cat)}`);
+  });
+}
+
+function fetchMealsByCategory() {
+  categories.forEach(cat => {
+    // filter by category
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat['strCategory']}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(`${cat['strCategory']} -> ${JSON.stringify(res)}`);
+      });
+  })
+}
+
+function abc(data) {
+  document.getElementById('container');
+  container.innerHTML = `
+			<span>${data.value}</span>
+			<small></small>
+			<div class="percent-bar"></div>
+		`;
+}
 // basic structure of the app:
 // list all categories
 // loop cats, show meals in each cat
