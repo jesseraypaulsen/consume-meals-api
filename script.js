a89545a4a7cfe2832e638ead51b2e74403ea2045
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-04-05 16:20:23 -0500

    categories

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 
+// https://www.themealdb.com/api.php
 document.body.onload = consume;
 
 function consume() {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`)
+  const categories = [];
+  // list all categories
+  fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
     .then(res => res.json())
     .then(res => {
-      res = JSON.stringify(res);
-      console.log(`the script has loaded! and here are the results: ${res}`);
+      //console.log(`the script has loaded! and here are the results: ${JSON.stringify(res)}`);
+      res.meals.forEach((cat, i) => {
+        categories.push(cat);
+        console.log(`${++i}. ${JSON.stringify(cat)}`);
+      });
     })
     .catch(err => {
       console.warn(err);
     });
 }
+
+// basic structure of the app:
+// list all categories
+// loop cats, show meals in each cat
+// display meals in flexbox container
+// give each meal the same event listener
+// expand a meal to see its detail in its own isolated space
+
+// a select menu for cats
+// one of the options selected by default, with meals in this cat showing onload
