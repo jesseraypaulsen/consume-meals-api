ab0ccc51d4fff1e6a078b53d4a41dab0dcd50fb1
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-04-06 12:53:39 -0500

    select menu where options are categories

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 
 // https://www.themealdb.com/api.php
 document.body.onload = consume;
-
-const categories = [];
+const el = document.querySelector('#container');
 
 function consume() {
+  getCategories();
+  
+  
+  //this is originally where I was going to call createMenu() -- but that doesn't work.
+  //due to asynchronous behavior, createMenu() must be called from within a promise chain.
+
+}
+
+function getCategories() {
   // list all categories
   fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
     .then(res => res.json())
     .then(res => {
-      processCategories(res.meals);
-      fetchMealsByCategory();
-    })
+      return processCategories(res.meals);
+      //fetchMealsByCategory();       //if something gets returned here, and we chain another promise,
+    })                                //does the returned value become the arg for the next promise?
+    .then(cats => createMenu(cats))
     .catch(err => {
       console.warn(err);
     });
-
-  
 }
 
 function processCategories(catArray) {
-  catArray.forEach((cat, i) => {
-    categories.push(cat);
+  return catArray.map((cat, i) => {
     console.log(`${++i}. ${JSON.stringify(cat)}`);
+    return cat['strCategory'];
   });
 }
 
-function fetchMealsByCategory() {
+//create select menu where categories are the options
+function createMenu(categories) {
+  const menu = document.createElement('select');
   categories.forEach(cat => {
-    // filter by category
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat['strCategory']}`)
-      .then(res => res.json())
-      .then(res => {
-        console.log(`${cat['strCategory']} -> ${JSON.stringify(res)}`);
-      });
-  })
+    const option = document.createElement('option');
+    option.value = cat;
+    option.innerText = cat;
+    menu.appendChild(option);
+  });
+  el.appendChild(menu);
 }
 
-function abc(data) {
-  document.getElementById('container');
-  container.innerHTML = `
-			<span>${data.value}</span>
-			<small></small>
-			<div class="percent-bar"></div>
-		`;
-}
-// basic structure of the app:
-// list all categories
-// loop cats, show meals in each cat
-// display meals in flexbox container
-// give each meal the same event listener
-// expand a meal to see its detail in its own isolated space
-
-// a select menu for cats
-// one of the options selected by default, with meals in this cat showing onload
+// function fetchMealsByCategory() {
+//   categories.forEach(catObj => {
+//     // filter by category
+//     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${catObj['strCategory']}`)
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(`${catObj['strCategory']} -> ${JSON.stringify(res)}`);
+//       });
+//   })
+// }
